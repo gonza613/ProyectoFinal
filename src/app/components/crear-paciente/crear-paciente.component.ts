81662d9ff1a01e1e335099dcb9e433c6081ceae4
unknown <gonzalo.lodigiani@i2t-sa.com.ar>
2024-10-09 12:43:47 -0300

    hiciste cagada

--- src/app/components/crear-paciente/crear-paciente.component.ts
+++ src/app/components/crear-paciente/crear-paciente.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Data } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-crear-paciente',
@@ -12,16 +14,19 @@ export class CrearPacienteComponent {
 
   constructor(
     private fb: FormBuilder,
-    private dialogRef: MatDialogRef<CrearPacienteComponent>
+    private dialogRef: MatDialogRef<CrearPacienteComponent>,
+    private login: LoginService
   ) {
     this.pacienteForm = this.fb.group({
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
+      usuario: ['', Validators.required],
       dni: ['', Validators.required],
       mail: ['', [Validators.required, Validators.email]],
       telefono: ['', Validators.required],
       contrasenia: ['', Validators.required],
-      confirmarContrasenia: ['', Validators.required]
+      confirmarContrasenia: ['', Validators.required],
+      fechanac: ['', Validators.required],
     });
   }
 
@@ -35,8 +40,23 @@ export class CrearPacienteComponent {
 
   onSubmit(): void {
     if (this.pacienteForm.valid) {
-      console.log(this.pacienteForm.value);
-      this.dialogRef.close(this.pacienteForm.value);
+      
+      let body = {
+        dni : this.pacienteForm.controls['dni'].value,
+        apellido: this.pacienteForm.controls['apellido'].value,
+        nombre: this.pacienteForm.controls['nombre'].value,
+        fecha_nacimiento: this.pacienteForm.controls['fechanac'].value,
+        password: this.pacienteForm.controls['contrasenia'].value,
+        rol: 'Paciente',
+        email: this.pacienteForm.controls['mail'].value,
+        telefono:this.pacienteForm.controls['telefono'].value,
+        // usuario: this.pacienteForm.controls['usuario'].value
+      }
+
+      this.login.register(JSON.stringify(body)).subscribe((data : any) =>{
+        console.log('Registro exitoso');
+        this.dialogRef.close(this.pacienteForm.value);
+      })
     }
   }
 }
